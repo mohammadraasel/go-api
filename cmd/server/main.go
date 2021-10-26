6b6bd3541e306a63d69137a01eb1b3f7d52f4619
Mohammad Rasel <mohammadrasel1221@gmail.com>
2021-10-27 01:31:49 +0600

    Setup router and add health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	tHttp "github.com/mohammadraasel/go-api/internal/transport/http"
+)
+
+type App struct {
+}
+
+func (app *App) Run() error {
+	handler := tHttp.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":4000", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our API")
+	}
 }
---
+++ internal/transport/http/handler.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	Router *mux.Router
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) SetupRoutes() {
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "I am alive!")
+	})
+}
